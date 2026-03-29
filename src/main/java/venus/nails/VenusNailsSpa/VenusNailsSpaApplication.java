cf9de6aeda56a25c6549fcb8e198f68919d2964c
Carolina Diaz <c.diazrojas0905@gmail.com>
2026-03-29 15:53:22 -0500

    config: eliminar Spring Security y corregir configuracion de conexion MySQL

--- src/main/java/venus/nails/VenusNailsSpa/VenusNailsSpaApplication.java
+++ src/main/java/venus/nails/VenusNailsSpa/VenusNailsSpaApplication.java
@@ -1,13 +1,9 @@
-package venus.nails.VenusNailsSpa;
-
+package venus.nails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class VenusNailsSpaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VenusNailsSpaApplication.class, args);
 	}
-
 }
