a795d204796dc54afcf459ac329f17ca757b2b62
Carolina Diaz <c.diazrojas0905@gmail.com>
2026-03-29 12:22:37 -0500

    init: proyecto Spring Boot con dependencias base

---
+++ src/main/java/venus/nails/VenusNailsSpa/VenusNailsSpaApplication.java
@@ -0,0 +1,13 @@
+package venus.nails.VenusNailsSpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusNailsSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenusNailsSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/venus/nails/VenusNailsSpa/VenusNailsSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package venus.nails.VenusNailsSpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusNailsSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
