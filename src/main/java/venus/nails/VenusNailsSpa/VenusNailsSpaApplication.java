18d2a0426e80b61167b5d7367cbba44767837611
Carolina Diaz <c.diazrojas0905@gmail.com>
2026-04-03 22:42:39 -0500

    Fix: corregir paquete VenusNailsSpaApplication

--- src/main/java/venus/nails/VenusNailsSpa/VenusNailsSpaApplication.java
+++ src/main/java/venus/nails/VenusNailsSpa/VenusNailsSpaApplication.java
@@ -1,4 +1,4 @@
-package venus.nails;
+package venus.nails.VenusNailsSpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
